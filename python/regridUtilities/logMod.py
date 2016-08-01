6b31e1ba8ee8e22f5e1b80574ec3a6d339f9811c
Logan Karsten <karsten@vpn84.ucar.edu>
2016-07-31 18:41:54 -0600

    Commit 7/31/2016

--- python/regridUtilities/logMod.py
+++ python/regridUtilities/logMod.py
@@ -7,11 +7,9 @@ MPI_COMM = MPI.COMM_WORLD
 MPI_RANK = MPI_COMM.Get_rank()
 
 def initLog(pId,cDate,logDir):
-    print MPI_RANK
-    print str(MPI_RANK)
     # Create log file path
-    logPath = logDir + "/REGRID_JOB_RANK" + str(MPI_RANK) + str(pId) + \
-              "_" + cDate + ".LOG"
+    logPath = logDir + "/REGRID_JOB_RANK" + str(MPI_RANK) + "_PID" + \
+              str(pId) + "_" + cDate + ".LOG"
     print logPath
     logging.basicConfig(filename=logPath,level=logging.DEBUG)
         
